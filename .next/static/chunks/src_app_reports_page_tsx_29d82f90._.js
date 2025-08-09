781ca44cbc492de1b123ef1ca94f33fdf0a218f2
adahdivergence <adahdivergence777@gmail.com>
2025-08-09 15:44:05 +0400

    Add .next/static/chunks/src_app_reports_page_tsx_29d82f90._.js 1754739844491

---
+++ .next/static/chunks/src_app_reports_page_tsx_29d82f90._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_0891ccc4._.js",
+  "static/chunks/node_modules_97df70c0._.js"
+],
+    source: "dynamic"
+});
