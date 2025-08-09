3a67aae2817578ada3b5ed6f89a3ad169a63ce54
adahdivergence <adahdivergence777@gmail.com>
2025-08-09 15:44:27 +0400

    Add .next/static/chunks/src_app_invoice_page_tsx_29d82f90._.js 1754739866447

---
+++ .next/static/chunks/src_app_invoice_page_tsx_29d82f90._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_467dddd3._.js",
+  "static/chunks/node_modules_23bb0d4e._.js"
+],
+    source: "dynamic"
+});
